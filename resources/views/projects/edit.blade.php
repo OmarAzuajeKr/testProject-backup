b7762903cf557e57932e25697d4d32e21cdc568a
Omar Azuaje <oazuaje.0374@unimar.edu.ve>
2025-02-12 16:01:03 -0400

    Actualizado: Agregadas mejoras en el componente visual

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,21 +1,23 @@
+<!-- filepath: /c:/Users/analista.desarrollo/Downloads/testProject/testProject/resources/views/projects/edit.blade.php -->
 @extends('layout')
 
 @section('title', 'Editar Proyecto')
 
 @section('content')
-    <h1>Editar Proyecto</h1>
-
-    <a href="{{ route('projects.index') }}">Regresar</a>
-
-    @include('partials.session-status')
-
-    @include('partials.validation-errors')
-
-
-
-    <form method="POST" action="{{ route('projects.update', $project) }}">
-        @method('PATCH')
-        @include('projects.form')
-        <button>Actualizar</button>
-    </form>
+    <div class="containerContact">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="titleContact">Editar Proyecto</h1>
+                <a href="{{ route('projects.index') }}" class="btn btn-secondary text-white mb-3">Regresar</a>
+                @include('partials.session-status')
+                @include('partials.validation-errors')
+                <form method="POST" action="{{ route('projects.update', $project) }}" class="bg-white shadow rounded py-3 px-4">
+                    @csrf
+                    @method('PATCH')
+                    @include('projects.form')
+                    <button class="btn btn-primary text-white w-100 mt-3">Actualizar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
